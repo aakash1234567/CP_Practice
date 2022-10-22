deefa6ed302499d8423c2c79442916cd5afb6c57
aakash1234567 <44570564+aakash1234567@users.noreply.github.com>
2022-10-23 04:41:11 +0530

    Time: 52 ms (54.55%), Space: 13.9 MB (12.37%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,10 +1,5 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        def solve(n):
-            if dp[n]!=-1:
-                return dp[n]
-            dp[n] = solve(n-1)+solve(n-2)+solve(n-3)
-            return dp[n]
         if n==0:
             return 0
         if n==1:
@@ -15,5 +10,6 @@ class Solution:
         dp[0]=0
         dp[1]=1
         dp[2]=1
-        
-        return solve(n)
+        for i in range(3,n+1):
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
+        return dp[n]
